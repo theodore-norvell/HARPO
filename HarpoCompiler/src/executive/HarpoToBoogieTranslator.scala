7de36b936665c1e145b02d2312e0c0621e268878
Inaam-Ahmed <inaama@mun.ca>
2019-06-12 17:42:49 -0230

    Build error removed. runVerifier() method is set up. Verification erros are printed out accordinly.

--- HarpoCompiler/src/executive/HarpoToBoogieTranslator.scala
+++ HarpoCompiler/src/executive/HarpoToBoogieTranslator.scala
@@ -3,80 +3,117 @@ import scala.collection.mutable.ArrayBuffer;
 import java.io.PrintWriter;
 import java.io.File;
 import frontEnd._
-import parser._ 
-import checker.Checker ;
-import scala.collection.mutable.ArrayBuffer 
-import java.io.StringReader 
+import parser._
+import checker.Checker;
+import scala.collection.mutable.ArrayBuffer
+import java.io.StringReader
 import boogieBackEnd.BoogieBackEnd
 import util.OutputBuilder;
+import boogieBackEnd.VerificationReport;
+import boogieBackEnd.BoogieErrorParser;
+import scala.sys.process.ProcessLogger;
+import scala.sys.process.Process;
+
 
 class HarpoToBoogieTranslator {
-    
-    private var errorRecorder = new StandardErrorRecorder()
-    
-    private var boogieOutput = "No Output"
-    
-        
-    def getErrorReport() : ErrorReport = errorRecorder 
-    
-    def getBoogieOutput(outputBuffer:OutputBuilder) : OutputBuilder = {
-      outputBuffer
-    }
-    
-    private var files = new ArrayBuffer[(String,String)] 
-    
-    def addFile( fileName : String, fileText : String ) {
-        files += ((fileName, fileText))
-    }
-    
-    def runHarpoToBoogieTrans( verbose : Boolean) {
-      
-        var transOutBuffer = new OutputBuilder 
-        errorRecorder = new StandardErrorRecorder()
-        println("Translator is running")
-        boogieOutput = ""
-        val masterDeclList  = new frontEnd.AST.DeclList() 
-        
-        for( (fileName, fileText) <- files ) {
-            	val reader  = new StringReader(fileText) 
-            	val p : HarpoParser = new HarpoParser( reader )
-              p.setFileName(fileName);
-            	val builder = new frontEnd.Builder(errorRecorder) ;
-            	p.setBuilder( builder )
-            	// Run the parser.
-    
-            	val dl : frontEnd.AST.DeclList =
-            	    try {
-            	        p.Start().asInstanceOf[frontEnd.AST.DeclList] }
-            	    catch{
-                        case ex : ParseException => {    
-            		        val coord = if( ex.currentToken != null ) AST.Coord( fileName, ex.currentToken.beginLine, ex.currentToken.beginColumn) 
-            		            		else AST.Coord( fileName) ;
-            		        errorRecorder.reportFatal(ex.getMessage(), coord)
-            		        null }
-            	        case ex : TokenMgrError => {    
-            	    	    val coord = AST.Coord( fileName )//TODO add line and column number
-            			    errorRecorder.reportFatal(ex.getMessage(), coord)
-            			    null } }
-            	  println(fileName,fileText,dl)
-                if( dl != null ) {
-                    for( decl <- dl.decls ) {
-                        masterDeclList.addDeclaration(decl) 
-                        } }
+
+  private var errorRecorder = new StandardErrorRecorder()
+
+  private var boogieOutput = "No Output"
+
+  def getErrorReport(): ErrorReport = errorRecorder
+
+  def getBoogieOutput(outputBuffer: OutputBuilder): OutputBuilder = {
+    outputBuffer
+  }
+
+  private var files = new ArrayBuffer[(String, String)]
+
+  def addFile(fileName: String, fileText: String) {
+    files += ((fileName, fileText))
+  }
+
+  def runHarpoToBoogieTrans(verbose: Boolean) = {
+
+    var transOutBuffer = new OutputBuilder
+
+    errorRecorder = new StandardErrorRecorder()
+
+    println("Translator is running")
+
+    val masterDeclList = new frontEnd.AST.DeclList()
+
+    for ((fileName, fileText) <- files) {
+      val reader = new StringReader(fileText)
+      val p: HarpoParser = new HarpoParser(reader)
+      p.setFileName(fileName);
+      val builder = new frontEnd.Builder(errorRecorder);
+      p.setBuilder(builder)
+      // Run the parser.
+
+      val dl: frontEnd.AST.DeclList =
+        try {
+          p.Start().asInstanceOf[frontEnd.AST.DeclList]
+        } catch {
+          case ex: ParseException => {
+            val coord = if (ex.currentToken != null) AST.Coord(fileName, ex.currentToken.beginLine, ex.currentToken.beginColumn)
+            else AST.Coord(fileName);
+            errorRecorder.reportFatal(ex.getMessage(), coord)
+            null
+          }
+          case ex: TokenMgrError => {
+            val coord = AST.Coord(fileName) //TODO add line and column number
+            errorRecorder.reportFatal(ex.getMessage(), coord)
+            null
+          }
         }
-        if( errorRecorder.getFatalCount() == 0) {
-             println("Checker Start");
-            val checker = new Checker( errorRecorder )
-            try { checker.runChecker( masterDeclList ) }
-            // The only exception that the checker should throw is a CompilerBailOutException.
-            catch{ case e : CompilerBailOutException => () } }
-        errorRecorder.printErrors( Console.out ) ;
-        // The boogie code generator
-        if( errorRecorder.getFatalCount() == 0) {
-           println("Fetal Errors= ", errorRecorder.getFatalCount());
-           val boogieCodeGen=new BoogieBackEnd(masterDeclList, transOutBuffer)
-           transOutBuffer = boogieCodeGen.getBoogieCode();
-           }
-        (errorRecorder, transOutBuffer)
+      println(fileName, fileText, dl)
+      if (dl != null) {
+        for (decl <- dl.decls) {
+          masterDeclList.addDeclaration(decl)
+        }
+      }
     }
+    if (errorRecorder.getFatalCount() == 0) {
+      println("Checker Start");
+      val checker = new Checker(errorRecorder)
+      try { checker.runChecker(masterDeclList) }
+      // The only exception that the checker should throw is a CompilerBailOutException.
+      catch { case e: CompilerBailOutException => () }
+    }
+    errorRecorder.printErrors(Console.out);
+    // The boogie code generator
+    if (errorRecorder.getFatalCount() == 0) {
+      println("Fetal Errors= ", errorRecorder.getFatalCount());
+      val boogieCodeGen = new BoogieBackEnd(masterDeclList, transOutBuffer)
+      transOutBuffer = boogieCodeGen.getBoogieCode();
+    }
+    (errorRecorder, transOutBuffer)
+  }
+
+  def runVerifer(text: String, verbose: Boolean): VerificationReport = {
+
+    val command = "boogie " + text;
+    /*
+   * without c process will hang
+   * ! will give back result
+   * !! gives back output in result
+   */
+
+    var log: String = "";
+
+    var result: String = "";
+
+    val logger = ProcessLogger((out) => result = out, (err) => log = err)
+
+    val output = Process(command).!(logger)
+
+    val outputDes = Process(command).!!
+
+    val ep: BoogieErrorParser = new BoogieErrorParser;
+
+    ep.parseBoogieOutput(outputDes)
+
+  }
+
 }
--- HarpoCompiler/src/executive/HarpoVerifier.scala
+++ HarpoCompiler/src/executive/HarpoVerifier.scala
@@ -13,30 +13,51 @@ import boogieBackEnd.VerificationReport;
 import boogieBackEnd.BoogieErrorParser;
 
 object HarpoVerifier extends App {
-    val verbose : Boolean = true
-    println( "\n\n\nVerifier start" )
-    var translator = new HarpoToBoogieTranslator()
-    translator.addFile( "HarpoSourceCode.harpo", getHarpoSource() )
-    val ( errorRecorder, transBuffer ) = translator.runHarpoToBoogieTrans( verbose )
 
-    if ( errorRecorder.getFatalCount() == 0 ) {
-        transBuffer.newLine
-        val text : String = transBuffer.result().mkString( "\n" )
-        val vr : VerificationReport = translator.runVerifer( text, verbose )
-        // TODO process the report.
+  val verbose: Boolean = true
+
+  var translator = new HarpoToBoogieTranslator()
+  translator.addFile("HarpoSourceCode.harpo", getHarpoSource())
+
+  val (errorRecorder, transBuffer) = translator.runHarpoToBoogieTrans(verbose)
+
+  if (errorRecorder.getFatalCount() == 0) {
+    transBuffer.newLine
+    val text: String = transBuffer.result().mkString("\n")
+    val vr: VerificationReport = translator.runVerifer(text, verbose)
+
+    if (vr.verificationErrorCount == 0) {
+      // TODO no verification error found
+    } else {
+      for (error <- vr.verificationErrorList) {
+        println(error.lineNumber + " " + error.boogieMessage + " caused by " + transBuffer.getError(error.lineNumber))
+
+      }
     }
+    // TODO process the report.
+    if (vr.otherErrorCount == 0) {
+      // TODO no other error found
+    } else {
+      for (error <- vr.otherErrorList) {
+        println(error.lineNumber + " " + error.boogieMessage)
 
-    errorRecorder.printErrors( System.out )
-    
-    println( "\n\n\nVerifier end" )
-
-    def getHarpoSource() : String = {
-        val sourceUrl : URL = this.getClass().getResource( "/ioSourceFiles/HarpoSourceCode.text" )
-        val harpoSource = new File( sourceUrl.toURI() )
-        val contents = Source.fromFile( harpoSource )
-        val sourceString = try contents.mkString finally contents.close()
-        return sourceString
+      }
     }
+    // TODO process the report.
+  }
+
+  errorRecorder.printErrors(System.out)
+
+  def getHarpoSource(): String = {
+    val sourceUrl: URL = this.getClass().getResource("/ioSourceFiles/HarpoSourceCode.text")
+    val harpoSource = new File(sourceUrl.toURI())
+    val contents = Source.fromFile(harpoSource)
+    val sourceString = try contents.mkString finally contents.close()
+    return sourceString
+  }
+
+
+
 }
 
 
