a3913f8eaa78766b4e80c9c4e91cf647fb735ca3
Inaam-Ahmed <inaama@mun.ca>
2019-08-06 16:19:58 -0230

    Indent Fixed For BuilderTypes. No important Change.

--- HarpoCompiler/src/boogieBackEnd/BuilderTypes.scala
+++ HarpoCompiler/src/boogieBackEnd/BuilderTypes.scala
@@ -3,15 +3,17 @@ import scala.collection.mutable.HashMap
 import checker.CheckerTypes.PrimitiveType
 
 object BuilderTypes {
-  val hToBoogieTypeMap = 
-HashMap(checker.CheckerTypes.int8->"int",
-		checker.CheckerTypes.int16->"int",
-		checker.CheckerTypes.int32->"int",
-		checker.CheckerTypes.int64->"int",
-		checker.CheckerTypes.real16->"real",
-		checker.CheckerTypes.real32->"real",
-    checker.CheckerTypes.real64->"real",
-		checker.CheckerTypes.bool->"bool")
+  val hToBoogieTypeMap =
+    HashMap(
+              checker.CheckerTypes.int8 -> "int",
+              checker.CheckerTypes.int16 -> "int",
+              checker.CheckerTypes.int32 -> "int",
+              checker.CheckerTypes.int64 -> "int",
+              checker.CheckerTypes.real16 -> "real",
+              checker.CheckerTypes.real32 -> "real",
+              checker.CheckerTypes.real64 -> "real",
+              checker.CheckerTypes.bool -> "bool"
+           )
 }
 
 //Out of range integers for the Boogie, we should not just convert the int1,16,32,64 into int in Boogie
