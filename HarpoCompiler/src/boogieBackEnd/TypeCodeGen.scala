9dc5662cf14d1d151952ab06a42b445845e7f99c
Inaam-Ahmed <inaama@mun.ca>
2019-08-06 16:30:36 -0230

    Extracting types while genedrating the defindness of expression i.e assert the range of values of names having types (Int8,Int16,Int32,Int64, Float16,Float32,Float64,bool) in between minimum and maximum range of numbers defined in Bogie prelude.

--- HarpoCompiler/src/boogieBackEnd/TypeCodeGen.scala
+++ HarpoCompiler/src/boogieBackEnd/TypeCodeGen.scala
@@ -1,54 +1,52 @@
 package boogieBackEnd
-
 import frontEnd.AST._
 import java.io._
 import checker.Checker
 import checker.CheckerTypes._
-import contracts.Contracts._ 
-
+import contracts.Contracts._
 import frontEnd._
-
 import frontEnd.AST._
 import java.io._
 import checker.Checker
 import checker.CheckerTypes._
-import contracts.Contracts._ 
+import contracts.Contracts._
 
 /**
  * @author Inaam Ahmed
  */
 object TypeCodeGen {
-    
-  private def build(ty:TypeNd): String = {
+
+  private def build(ty: TypeNd): String = {
     check(ty.tipe.isInstanceOf[Some[Type]])
     val baseType = extractBaseType(ty.tipe.get)
-    
-    var codeType : String = 
-        if (baseType.isInstanceOf[PrimitiveType] ) {
-            val primType = baseType.asInstanceOf[PrimitiveType]
-            check( BuilderTypes.hToBoogieTypeMap.contains( primType ) )
-            BuilderTypes.hToBoogieTypeMap(primType) }
-        else {
-            unreachable("Bad base type in TypeCodeGen." ) ; }
-    
+    var codeType: String =
+      if (baseType.isInstanceOf[PrimitiveType]) {
+        val primitiveType = baseType.asInstanceOf[PrimitiveType]
+        check(BuilderTypes.hToBoogieTypeMap.contains(primitiveType))
+        BuilderTypes.hToBoogieTypeMap(primitiveType)
+      } else {
+        unreachable("Bad base type in TypeCodeGen.");
+      }
     return codeType
   }
-    
-  private def extractBaseType(t : Type) : Type = {
-      t match{
-        case ClassIntfType( decl : AST.ClassLikeDeclNd ) => t
 
-        case PrimitiveType( name : QN ) => t
-        
-        case ArrayType( base : Type, bound : AST.ExpNd ) =>  extractBaseType(base)
-        
-        case LocationType( base : PrimitiveType ) => extractBaseType(base)
-        
-        case _ => unreachable("Unexpected type in extractBaseType.")
+  private def extractBaseType(tipe: Type): Type = {
+    tipe match {
+      case ClassIntfType(decl: AST.ClassLikeDeclNd) => tipe
+
+      case PrimitiveType(name: QN) => tipe
+
+      case ArrayType(base: Type, bound: AST.ExpNd) => extractBaseType(base)
+
+      case LocationType(base: PrimitiveType) => extractBaseType(base)
+
+      case ArrayLocationType(baseType: ArrayType) => extractBaseType(baseType)
+
+      case _ => unreachable("Unexpected type in extractBaseType.")
     }
   }
-  
-  def apply(ty : TypeNd) : String = {
+
+  def apply(ty: TypeNd): String = {
     build(ty)
   }
 }
